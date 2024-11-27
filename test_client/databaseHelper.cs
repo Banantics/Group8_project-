4e3fc5af6521d3a6c7f1a0e8c87b99ade12e71f4
Banantics <65262161+Banantics@users.noreply.github.com>
2024-11-27 09:57:05 +0100

    took out the bug where the end_device inserts duplicate devices

--- test_client/databaseHelper.cs
+++ test_client/databaseHelper.cs
@@ -14,11 +14,10 @@ namespace Client
 {
     public class databaseHelper
     {
-        
         public void insert(values weatherData)
         {
-            string connectionString = "Server=DESKTOP-BRPSKK1;Database=Weather;Trusted_Connection=True;";
-
+            // string connectionString = "Server=DESKTOP-BRPSKK1;Database=weather;Trusted_Connection=True;";
+            string connectionString = "Server=Alians;Database=weather;Trusted_Connection=True;";
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -26,27 +25,99 @@ namespace Client
                     connection.Open();
                     Console.WriteLine("Connected to SQL Server.");
 
-                    string query = @"
-                INSERT INTO WeatherData (City, Time, Temperature, AmbientLight, Humidity)
-                VALUES (@City, @Time, @Temperature, @AmbientLight, @Humidity)";
+                    string  checkDeviceQuery = "SELECT 1 FROM wet.end_device WHERE device_id = @DeviceId";
+                    bool deviceExists = false;
 
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    using (SqlCommand checkCommand = new SqlCommand(checkDeviceQuery, connection))
                     {
-                        command.Parameters.AddWithValue("@City", weatherData.AplicationId);
-                        command.Parameters.AddWithValue("@Time", weatherData.Time);
-                        command.Parameters.AddWithValue("@Temperature", weatherData.Temperature);
-                        command.Parameters.AddWithValue("@AmbientLight", weatherData.AmbientLight);
-                        command.Parameters.AddWithValue("@Humidity", weatherData.Humidity);
+                        checkCommand.Parameters.AddWithValue("@DeviceId", weatherData.DeviceId);
+                        object result = checkCommand.ExecuteScalar();
+                        deviceExists = result != null;
+                    }
 
+                    if (!deviceExists)
+                    {
+
+                        // End_device
+                        string insertEndDeviceQuery = @"
+                        INSERT INTO wet.end_device (device_id, application, devEUI, joinEUI, dev_address)
+                        VALUES (@DeviceId, @ApplicationId, @DevEUI, @JoinEUI, @DevAddress)";
+
+                        using (SqlCommand command = new SqlCommand(insertEndDeviceQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@DeviceId", weatherData.DeviceId);
+                            command.Parameters.AddWithValue("@ApplicationId", weatherData.ApplicationId);
+                            command.Parameters.AddWithValue("@DevEUI", weatherData.DevEUI);
+                            command.Parameters.AddWithValue("@JoinEUI", weatherData.JoinEUI);
+                            command.Parameters.AddWithValue("@DevAddress", weatherData.DevAddress);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Device with ID {weatherData.DeviceId} already exists in the end_device table. Skipping insertion.");
+                    }
+
+                        // Uplink_messages
+                        string insertUplinkMessagesQuery = @"
+                INSERT INTO wet.uplink_messages (device_id, sesion_key_id, FPort, FCnt, frm_Payload, humidity, pressure, temperature, timestamp)
+                VALUES (@DeviceId, @SessionKeyId, @FPort, @FCnt, @FrmPayload, @Humidity, @Pressure, @Temperature, @Timestamp)";
+
+                    using (SqlCommand command = new SqlCommand(insertUplinkMessagesQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@DeviceId", weatherData.DeviceId);
+                        command.Parameters.AddWithValue("@SessionKeyId", weatherData.SessionKeyId);
+                        command.Parameters.AddWithValue("@FPort", weatherData.FPort);
+                        command.Parameters.AddWithValue("@FCnt", weatherData.FCnt);
+                        command.Parameters.AddWithValue("@FrmPayload", weatherData.FrmPayload);
+                        command.Parameters.AddWithValue("@Humidity", weatherData.Humidity);
+                        command.Parameters.AddWithValue("@Pressure", weatherData.Pressure);
+                        command.Parameters.AddWithValue("@Temperature", weatherData.Temperature);
+                        command.Parameters.AddWithValue("@Timestamp", weatherData.Timestamp);
+                        command.ExecuteNonQuery();
+                    }
+
+                    // Rx_metadata
+                    string insertRxMetadataQuery = @"
+                INSERT INTO wet.rx_metadata (message_id, gate_way_id, gate_way_EUI, time, timestamp, RSSI, channel_RSSI, SNR, latitude, longitude, altitude)
+                VALUES (SCOPE_IDENTITY(), @GatewayId, @GatewayEUI, @MetadataTime, @MetadataTimestamp, @RSSI, @ChannelRSSI, @SNR, @Latitude, @Longitude, @Altitude)";
+
+                    using (SqlCommand command = new SqlCommand(insertRxMetadataQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@GatewayId", weatherData.GatewayId);
+                        command.Parameters.AddWithValue("@GatewayEUI", weatherData.GatewayEUI);
+                        command.Parameters.AddWithValue("@MetadataTime", weatherData.MetadataTime);
+                        command.Parameters.AddWithValue("@MetadataTimestamp", weatherData.MetadataTimestamp);
+                        command.Parameters.AddWithValue("@RSSI", weatherData.RSSI);
+                        command.Parameters.AddWithValue("@ChannelRSSI", weatherData.ChannelRSSI);
+                        command.Parameters.AddWithValue("@SNR", weatherData.SNR);
+                        command.Parameters.AddWithValue("@Latitude", weatherData.Latitude);
+                        command.Parameters.AddWithValue("@Longitude", weatherData.Longitude);
+                        command.Parameters.AddWithValue("@Altitude", weatherData.Altitude);
+                        command.ExecuteNonQuery();
+                    }
+
+                    // Setting
+                    string insertSettingsQuery = @"
+                INSERT INTO wet.settings (message_id, bandwith, spreading_factor, coding_rate, frequency, timestamp)
+                VALUES (SCOPE_IDENTITY(), @Bandwidth, @SpreadingFactor, @CodingRate, @Frequency, @SettingsTimestamp)";
+
+                    using (SqlCommand command = new SqlCommand(insertSettingsQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Bandwidth", weatherData.Bandwidth);
+                        command.Parameters.AddWithValue("@SpreadingFactor", weatherData.SpreadingFactor);
+                        command.Parameters.AddWithValue("@CodingRate", weatherData.CodingRate);
+                        command.Parameters.AddWithValue("@Frequency", weatherData.Frequency);
+                        command.Parameters.AddWithValue("@SettingsTimestamp", weatherData.SettingsTimestamp);
                         command.ExecuteNonQuery();
                     }
                 }
 
-                Console.WriteLine("Data inserted successfully.");
+                Console.WriteLine("inserted succesfyllu.");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error inserting data into database: {ex.Message}");
+                Console.WriteLine($"Error inserting data : {ex.Message}");
             }
         }
     }
--- test_client/values.cs
+++ test_client/values.cs
@@ -11,12 +11,42 @@ namespace vals
 {
     public class values
     {
-        public string AplicationId { get; set; }
-        public DateTime Time { get; set; }
-        public double Temperature { get; set; }
-        public double AmbientLight { get; set; }
-        public double Humidity { get; set; }
+        // 'end_device_ids'
+        public string DeviceId { get; set; }
+        public string ApplicationId { get; set; }
+        public string DevEUI { get; set; }
+        public string JoinEUI { get; set; }
+        public string DevAddress { get; set; }
 
+        // 'uplink_message'
+        public string SessionKeyId { get; set; }
+        public int FPort { get; set; }
+        public int FCnt { get; set; }
+        public string FrmPayload { get; set; }
+        public double Humidity { get; set; }
+        public double Light { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        // 'rx_metadata'
+        public string GatewayId { get; set; }
+        public string GatewayEUI { get; set; }
+        public DateTime MetadataTime { get; set; }
+        public long MetadataTimestamp { get; set; }
+        public double RSSI { get; set; }
+        public double ChannelRSSI { get; set; }
+        public double SNR { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+
+        // 'settings'
+        public int Bandwidth { get; set; }
+        public int SpreadingFactor { get; set; }
+        public string CodingRate { get; set; }
+        public string Frequency { get; set; }
+        public long SettingsTimestamp { get; set; }
     }
 }
 
